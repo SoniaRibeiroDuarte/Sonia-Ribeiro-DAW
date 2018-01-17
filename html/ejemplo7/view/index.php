f2283c313ad1ed045924dccefb3a1a7ff592aed5
web <web@5300101A10713>
2018-01-17 18:04:54 +0100

    commit inicial

---
+++ html/ejemplo7/view/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3><?php echo "el contacto es $datos->nombre $datos->apellido"?></h3>
+        <h4><?php echo "el email del contacto es $datos->email"?></h4>
+    </body>
+</html>
