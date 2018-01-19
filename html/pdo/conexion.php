0ba869458d7cc53d8f14953446279ae96297d81a
web <web@5300101A10713>
2018-01-19 18:07:03 +0100

    commit inicial

--- html/pdo/conexion.php
+++ html/pdo/conexion.php
@@ -1,3 +1,10 @@
 <?php
 
-$db = new PDO('mysql:host=localhost;dbname=amazonAWS', 'root', 'Okonomiyaki1');
+$opciones = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=amazonAWS', 'root', 'Okonomiyaki1', $opciones);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Falló la conexión: ' . $e->getMessage();
+}
