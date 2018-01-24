4843968db9897058d0ca86d5589938b9a4a00a8e
web <web@5300101A10713>
2018-01-24 18:16:36 +0100

    commit inicial

--- html/cms/view/index.php
+++ html/cms/view/index.php
@@ -7,7 +7,7 @@
     <body>
         <h3>Id <?php echo "$datos->id"?></h3>
         <h3>Usuario <?php echo "$datos->usuario"?></h3>
-        <h3>Clave<?php echo "$datos->clave"?></h3>
+        <h3>Clave <?php echo crypt($datos->clave,"hola")?></h3>
         <h3>Fecha acceso <?php echo "$datos->fecha_acceso"?></h3>
         <h3>Activo <?php echo "$datos->activo"?></h3>
         <h3>Usuario <?php echo "$datos->usuarios"?></h3>
